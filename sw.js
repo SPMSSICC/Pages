af08e4465b87c59d30deead2751003f3b76af674
SPMSSICC <spmsicc@gmail.com>
2019-09-16 16:58:57 +0100

    Atualização da cache

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 or its subdirectories. This means that if you place the service worker file in a scripts directory
 it will only be able to interact with pages in the scripts directory or below.*/
 
-var cacheName = 'spmssicc_v80/01/2018v88'; //Edit this to update cache
+var cacheName = 'spmssicc_v80/01/2018v89'; //Edit this to update cache
  var cacheFiles = [
     // '/pages/',
     // '/pages/index.html',
